884070cdf70419e7eaabd659efddbbd47c219ab1
Vitali Dolgoshei <Vitald75@gmail.com>
2021-10-13 21:59:42 +0300

    3 tasks hw1

---
+++ task2.js
@@ -0,0 +1,15 @@
+"use strict";
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Число " + i + " делится без остатка на 3 и на 5");
+    continue;
+  }
+  if (i % 3 == 0) {
+    console.log("Число " + i + " делится без остатка на 3");
+    continue;
+  }
+  if (i % 5 == 0) {
+    console.log("Число " + i + " делится без остатка на 5");
+    continue;
+  }
+}
---
+++ task3.js
@@ -0,0 +1,19 @@
+"use strict";
+const prices = [
+  64, 7556, 345, 7556, 345, 7556, 345, 7556, 433, 345, 756, 123, 942, 3112, 421,
+  9341, 1212, 8, 43, 41, 345, 341, 21, 321, 123,
+];
+
+let sum = 0;
+for (let i = 0; i < prices.length; i++) {
+  sum += prices[i];
+}
+
+let averagePrice = sum / prices.length;
+console.log(
+  "Итого: " +
+    sum +
+    " гривен, средняя цена товара " +
+    averagePrice.toFixed(2) +
+    " гривен"
+);
